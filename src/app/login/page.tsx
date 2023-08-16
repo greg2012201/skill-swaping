1a64228dca49434016458eee92844fa1c6ce72b1
Grzegorz Dubiel <greg2012201@gmail.com>
2023-08-17 01:14:49 +0200

    Prepare login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 function LoginPage() {
   return (
-    <div>page</div>
-  )
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-3">
+      <button type="button" className="btn btn-lg normal-case">
+        Sign in with GitHub
+        <FaGithub size={20} />
+      </button>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
