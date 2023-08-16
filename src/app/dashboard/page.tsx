4de141b016464bbc45d639be14ee4dd163d4e4e8
Grzegorz Dubiel <greg2012201@gmail.com>
2023-08-17 01:39:04 +0200

    Add logout.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { logout } from "@/auth";
 import React from "react";
 
 function DashboardPage() {
-  return <div>dashboard</div>;
+  return (
+    <div>
+      <button className="btn" onClick={() => logout()}>
+        logout
+      </button>
+    </div>
+  );
 }
 
 export default DashboardPage;
